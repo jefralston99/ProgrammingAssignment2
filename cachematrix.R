f8d1863ec82628dd973c712c57d4b2a4584cbb5f
jefralston99 <jeffrey.ralston@gmail.com>
2014-07-27 16:11:00 -0500

    Commit #2
    Updated commenting on each section of code

--- cachematrix.R
+++ cachematrix.R
@@ -2,11 +2,20 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function takes a matrix input (assumed to be a square matrix)
+## it returns a list of 4 items: set, get, setsolvedx, and getinv_x
+## The list values are used in the cachesolve() function
+
+## m is a matrix whose lexical scope is seen by both functions
+## due to the use of the <<- assignment operator
+## setsolvedx is the cached storage of the inverse of the input matrix
+## getinv_x is the instruction to recompute the inverse of input matrix
+## set and get do just as they describe
 
 makeCacheMatrix <- function(x = matrix()) {
     
     
-        m <- NULL
+        m <- NULL  #initializing m to contain no values
         set <- function(y) {
             x <<- y
             m <<- NULL
@@ -23,6 +32,9 @@ makeCacheMatrix <- function(x = matrix()) {
  
 
 ## Write a short comment describing this function
+## cacheSolve returns the cached inverse of x unless it is NULL
+## in which case it computes the inverse of x
+## and stores it for future cache uses
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
